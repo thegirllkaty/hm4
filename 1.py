847843bbac1c4d0d407ce20fb316f08b753fce38
theksenia <ksu.pm13@gmail.com>
2021-07-16 21:17:11 +0300

    add

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-print(hello)
+
+
+'''a = [1, 2, 1, 3, 5, 7, 4, 2, 8, 4, 3, 3]
+b = []
+
+for i in a:
+    if i not in b:
+        b.append(i)
+print(b)'''
+
+import random
+l= []
+for i in range(10):
+    l.append(random.randint(0,9))
+print(l)
+
+
