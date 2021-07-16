4d16f98f7566947574850b306113ca8e75792658
theksenia <ksu.pm13@gmail.com>
2021-07-16 21:23:28 +0300

    add

--- 1.py
+++ 1.py
@@ -1,17 +1,12 @@
-
-
-'''a = [1, 2, 1, 3, 5, 7, 4, 2, 8, 4, 3, 3]
-b = []
-
-for i in a:
-    if i not in b:
-        b.append(i)
-print(b)'''
-
 import random
-l= []
+l= [] ; m= []
 for i in range(10):
     l.append(random.randint(0,9))
 print(l)
+
+for j in l:
+    if j not in m:
+        m.append(j)
+print(m)
 
 
