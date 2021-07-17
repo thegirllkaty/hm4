6cf81d246849b029919ca566738795692f24b319
theksenia <ksu.pm13@gmail.com>
2021-07-17 06:30:26 +0300

    add

---
+++ 3.py
@@ -0,0 +1,16 @@
+import random
+m= []; l1= []; l2= []; l=[]
+for i in range(5):
+    l2.append(random.randint(0,2))
+print(l2)
+
+for p in range(3):
+    l1.append(random.randint(0,4))
+print(l1)
+
+for n in range(i+p):
+
+
+
+    
+
