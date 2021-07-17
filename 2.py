4f4c908e4cf6e7a3314b5b7b9b0c6ccbfa712b83
theksenia <ksu.pm13@gmail.com>
2021-07-17 06:08:35 +0300

    add

--- 2.py
+++ 2.py
@@ -4,7 +4,7 @@ for i in range(11):
     l.append(random.randint(0,105))
 print(l)
 
-L1=l  #список скопирован
+L1=l  #список скопирован  иначе      L1=l.copy()
 print(L1) #вывод скопированого списка
 
 
